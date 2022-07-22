df48c8ccc3ddcb610cb4aecc6cec44d509943c44
MUKANTWARI Françoise <f.mukantwari@alustudent.com>
2022-07-22 20:57:30 +0200

    ch(setup-react-project): Setup an empty react project
    ch(setup-react-project): Setup an empty react project
    
    ch(setup-react-project): Setup an empty react project
    
    ch(setup-react-project): Setup an empty react project
    
    ch(setup-react-project): Setup an empty react project
    
    ch(setup-react-project): Setup an empty react project
    
    ch(setup-react-project): Setup an empty react project
    
    ch(setup-react-project): Setup an empty react project
    
    ch(setup-react-project): Setup an empty react project
    
    ch(setup-react-project): Setup an empty react project
    
    ch(setup-react-project): Setup an empty react project
    
    ch(setup-react-project): Setup an empty react project
    
    ch(setup-react-project): Setup an empty react project
    
    chore:(setting scss and css):enable client to use css and scss
    
    chore(setting scss and css):enable user to use scss or css
    
    ch(setup-test): Set up tests on the client
    
    chore(circleci):setting circleci to run test

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/index.scss';
+function App() {
+  return (
+        <div>
+           <h1> Barefoot Nomad</h1>
+        </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+import './styles/index.scss';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App tab="home" />)
+
