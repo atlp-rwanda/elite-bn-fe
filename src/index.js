a6e2f28832531d140c9f95bfdb139bbea6ccba07
Julish <andelarwanda@Andelas-MacBook-Pro.local>
2022-07-28 00:43:47 +0200

    ch(set-up-redux)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import './styles/index.scss';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
-root.render(<App tab="home" />)
-
+root.render(
+    <Provider store={store}>
+        <App tab="home" />
+    </Provider>, 
+)
---
+++ webpack.config 2.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|ico)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'img/[name].[hash].[ext]',
+          esModule: false,
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          'html-loader',
+        ],
+      },
+      { test: /\.txt$/, use: 'raw-loader' },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
