0258f00cb147c68d0b05521bacd70af04744dcd0
KALISA_NGABO_KEVIN <kevinkalisa84@gmail.com>
2022-08-02 19:10:42 +0200

    ft(register-user):Users should be able Signup/Register to Barefoot Nomad

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import Login from '../Views/LoginView'
-import HomeView from '../Views/HomeView'
-import CounterView from '../Views/CounterView'
+import React from "react";
+import Login from "../Views/LoginView";
+import HomeView from "../Views/HomeView";
+import CounterView from "../Views/CounterView";
+import RegisterView from "../Views/RegisterView";
 
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 
-function AllRoutes() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<HomeView />} />
-        <Route path="login" element={<Login />} />
-        <Route path="count" element={<CounterView />} />
-      </Routes>
-    </div>
-  )
+
+function AllRoutes(){
+    return(
+        <div>
+          <Routes>
+            <Route path='/' element={<HomeView />} /> 
+            <Route path='register' element={<RegisterView />}/>
+            <Route path='login' element={ <Login />}/>
+            <Route path='count' element={ <CounterView />}/>
+          </Routes>
+        </div>
+    )
 }
 export default AllRoutes
