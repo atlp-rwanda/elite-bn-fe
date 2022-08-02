a012dc7b65327fc70f682134514cf194960ab97a
KALISA_NGABO_KEVIN <kevinkalisa84@gmail.com>
2022-08-02 19:10:42 +0200

    ft(register-user):Users should be able Signup/Register to Barefoot Nomad

---
+++ src/Views/RegisterView.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function RegisterView(){
+   return (
+       <div>
+           <h1>Register to barefoot</h1>
+       </div>
+   )
+}
+export default RegisterView
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./*.html', './src/**/*.js'],
+  theme: {
+    extend: {},
+    variants: {
+      extend: {},
+  },
+  plugins: [],
+}
