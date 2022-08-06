3e738f3b2bf20d929a026669a6cf5a90028508c5
Munyaneza Armel <munyaarmel61@gmail.com>
2022-08-06 13:26:37 +0000

    feat(axios):configure axios
    feat(Axios):configure axios
    
    feat(axios):configure axios to call api
    
    feat(axios):configure axios to call api

---
+++ utils/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const token=localStorage.getItem('token')
+
+export default axios.create({
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+    headers: {
+        'Authorization': 'Bearer ' + token
+      }
+
+})
