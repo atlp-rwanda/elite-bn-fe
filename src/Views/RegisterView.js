7f3036a3652ac2eb8ec6cebb01b5f501a2999a20
KALISA_NGABO_KEVIN <kevinkalisa84@gmail.com>
2022-08-06 22:33:55 +0200

    setup

--- src/Views/RegisterView.js
+++ src/Views/RegisterView.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Signup from '../components/Signup/signUp';
 
 function RegisterView(){
    return (
        <div>
            <h1>Register to barefoot</h1> 
+           <Signup/>
        </div>
    )
 }
-export default RegisterView
+export default RegisterView;
