e2bb4f6b11343a4123b0b13aa9d07cdccc01ebc6
KALISA_NGABO_KEVIN <kevinkalisa84@gmail.com>
2022-08-23 20:56:33 +0200

    ft(user-role-setting): User Role Settings UI

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ import CounterView from '../Views/CounterView'
 import ThemeView from '../Views/ThemeView'
 import RegisterView from '../Views/RegisterView'
 import DashboardView from '../Views/Dashboard'
-import PrivateRoutes from '../../utils/privateRoute'
+import UserRoleView from '../Views/userRolesView'
 
 function AllRoutes() {
   return (
@@ -17,9 +17,11 @@ function AllRoutes() {
           <Route path="count" element={<CounterView />} />
           <Route path="theme" element={<ThemeView />} />
           <Route path="dashboard" element={<DashboardView />} />
+        <Route path="roles" element={<UserRoleView />} />
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="register" element={<RegisterView />} />
+
       </Routes>
     </div>
   )
