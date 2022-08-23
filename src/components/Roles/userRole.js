77df4d7a9c085644e8b6f19ba42da33109e9e9f3
KALISA_NGABO_KEVIN <kevinkalisa84@gmail.com>
2022-08-23 23:35:52 +0200

    (ft-user-prifle): Table

--- src/components/Roles/userRole.js
+++ src/components/Roles/userRole.js
@@ -10,28 +10,10 @@ import TableRow from '@mui/material/TableRow';
 
 const columns = [
   { id: 'name', label: 'First name', minWidth: 170 },
-  { id: 'code', label: 'Last name', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Username',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Email',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Role',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
-  },
+  { id: 'code', label: 'Last name', minWidth: 170 },
+  { id: 'population', label: 'Username', minWidth: 170 },
+  { id: 'size', label: 'Email', minWidth: 170 },
+  { id: 'density', label: 'Role', minWidth: 170 },
 ];
 
 function createData(name, code, population, size) {
@@ -40,21 +22,7 @@ function createData(name, code, population, size) {
 }
 
 const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-//   createData('Italy', 'IT', 60483973, 301340),
-//   createData('United States', 'US', 327167434, 9833520),
-//   createData('Canada', 'CA', 37602103, 9984670),
-//   createData('Australia', 'AU', 25475400, 7692024),
-//   createData('Germany', 'DE', 83019200, 357578),
-//   createData('Ireland', 'IE', 4857000, 70273),
-//   createData('Mexico', 'MX', 126577691, 1972550),
-//   createData('Japan', 'JP', 126317000, 377973),
-//   createData('France', 'FR', 67022000, 640679),
-//   createData('United Kingdom', 'GB', 67545757, 242495),
-//   createData('Russia', 'RU', 146793744, 17098246),
-//   createData('Nigeria', 'NG', 200962417, 923768),
-//   createData('Brazil', 'BR', 210147125, 8515767),
+  createData('Kalisa', 'Ngabo', 'kevin', 'kengabo@gmail.com', 3287263),
 ];
 
 export default function StickyHeadTable() {
@@ -109,15 +77,6 @@ export default function StickyHeadTable() {
           </TableBody>
         </Table>
       </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
     </Paper>
   );
 }
