0435a482e311f13593b94c7a0826627a83edfab7
Munyaneza Armel <munyaarmel61@gmail.com>
2022-08-24 12:23:43 +0000

    feat(landing page):HOme page

--- src/Views/HomeView.js
+++ src/Views/HomeView.js
@@ -1,17 +1,12 @@
 import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import Grid from '@material-ui/core/Grid'
-import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-import { TextField } from '@material-ui/core'
+import Home from '../components/Home/home'
 
 function HomeView() {
   return (
     <div>
-      <h1>Welcome to barefoot</h1>
+
+      <Home />
+
     </div>
   )
 }
