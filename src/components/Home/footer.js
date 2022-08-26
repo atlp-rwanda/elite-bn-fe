88d00d5de0c9c4a21372214702f1afaf412eaf20
Munyaneza Armel <munyaarmel61@gmail.com>
2022-08-26 08:30:23 +0000

    feat(landing page):HOme page

---
+++ src/components/Home/footer.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Grid from '@material-ui/core/Grid'
+import Typography from '@mui/material/Typography';
+import useStyles from './styles'
+import Box from '@mui/material/Box';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import Avatar from '@mui/material/Avatar';
+import {StarBorderOutlined} from '@material-ui/icons';
+import Container from '@mui/material/Container';
+
+export default function Footer() {
+    const classes=useStyles()
+  return (
+  <Container>
+    
+  </Container>
+  );
+}
