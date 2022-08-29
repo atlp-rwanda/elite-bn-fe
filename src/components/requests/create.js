6e5cfe1c945bd8eab1930f3d7d33394217e17040
Munyaneza Armel <munyaarmel61@gmail.com>
2022-08-29 07:35:58 +0000

    feat(request):create and edit request

---
+++ src/components/requests/create.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+import '../styles/index.scss'
+
+function create() {
+  return (
+    <div>
+      <h1> Barefoot Nomad</h1>
+    </div>
+  )
+}
+
+export default create
