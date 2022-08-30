7adc0fcddbdbdcd8c43bed492ddb5b213cd8796e
KALISA_NGABO_KEVIN <kevinkalisa84@gmail.com>
2022-08-30 23:22:21 +0200

    user role setting

--- src/components/Roles/userRole.js
+++ src/components/Roles/userRole.js
@@ -17,25 +17,9 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { styled } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
-<<<<<<< Updated upstream
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import {
-  getProfileData,
-  updateProfileData,
-  thisUser,
-} from '../../redux/features/roleReducer';
-// import rows from "./rowData";
-
-=======
 
 
 
->>>>>>> Stashed changes
 const BootstrapInput = styled(InputBase)(({ theme }) => ({
   'label + &': {
     marginTop: theme.spacing(3),
@@ -92,61 +76,17 @@ export default function FormDialog() {
   const handleChange = (event) => {
     setRole(event.target.value);
   };
-<<<<<<< Updated upstream
-  const paperStyle = {
-    padding: 20,
-    height: '20%',
-    justifyContent: 'center',
-
-  }
-=======
-
->>>>>>> Stashed changes
   return (
     <Grid>
     <Box>
 
   <Stack spacing={2} direction="row" >      <ThemeProvider theme={theme}>
       <Button color="neutral" variant="contained" onClick={handleClickOpen}> Assign Role </Button>
-<<<<<<< Updated upstream
-        <Button color="neutral" variant="contained" >Logout</Button>
-        </ThemeProvider>
-  </Stack>
-  <div>
-    <Paper className="container" elevation={5} style={paperStyle}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>First Name</TableCell>
-            <TableCell>Last Name</TableCell>
-            <TableCell>Username</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Role</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {map(({ id, firstName, lastName, Username, email, Role }) => (
-            {/* {rows.map(({ id, firstName, lastName, Username, email, Role }) => ( */}
-            <TableRow key={id}>
-              <TableCell component="th" scope="row">
-                {firstName}
-              </TableCell>
-              <TableCell omponent="th" scope="row">{lastName}</TableCell>
-              <TableCell omponent="th" scope="row">{Username}</TableCell>
-              <TableCell omponent="th" scope="row">{email}</TableCell>
-              <TableCell omponent="th" scope="row">{Role}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
-=======
 
         </ThemeProvider>
   </Stack>
   <div>
 
->>>>>>> Stashed changes
   </div>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle align='center' color={Colors.primary}>Assign New Role</DialogTitle>
@@ -187,11 +127,6 @@ export default function FormDialog() {
         </DialogActions>
       </Dialog>
     </Box>
-<<<<<<< Updated upstream
-    </Grid>
-  );
-}
-=======
     <Box>
 
 <Stack spacing={2} direction="row" >      <ThemeProvider theme={theme}>
@@ -244,4 +179,3 @@ export default function FormDialog() {
     </Grid>
   );
 }
->>>>>>> Stashed changes
